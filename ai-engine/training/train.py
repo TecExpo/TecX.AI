e3bc44e359cbc473cbe4461f9f70e70537745e04
TecExpo <142141994+TecExpo@users.noreply.github.com>
2025-02-09 09:35:46 +0530

    Create train.py

---
+++ ai-engine/training/train.py
@@ -0,0 +1,4 @@
+│── ai-engine/            # AI Pipelines and Training
+│   ├── models/          # Pretrained AI models and training data
+│   ├── training/        # Scripts for AI model training and fine-tuning
+│   │   ├── train.py
