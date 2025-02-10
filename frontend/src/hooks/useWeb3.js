4a729ca4be9d13e49234d5f0867a7b1ebd553b78
TecExpo <142141994+TecExpo@users.noreply.github.com>
2025-02-11 00:27:24 +0530

    Update useWeb3.js

--- frontend/src/hooks/useWeb3.js
+++ frontend/src/hooks/useWeb3.js
@@ -1,1 +1,27 @@
+import { useState, useEffect } from "react";
+import provider from "../web3/provider";
+import { ethers } from "ethers";
 
+const useWeb3 = () => {
+  const [signer, setSigner] = useState(null);
+  const [balance, setBalance] = useState("0");
+
+  useEffect(() => {
+    const fetchSigner = async () => {
+      if (provider) {
+        const signer = await provider.getSigner();
+        setSigner(signer);
+
+        const address = await signer.getAddress();
+        const balanceWei = await provider.getBalance(address);
+        setBalance(ethers.formatEther(balanceWei));
+      }
+    };
+
+    fetchSigner();
+  }, []);
+
+  return { signer, balance };
+};
+
+export default useWeb3;
