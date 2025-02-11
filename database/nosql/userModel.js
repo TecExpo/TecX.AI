00bda02881b62ec5bf1e6ea7eebfa1aa2e758db1
TecExpo <142141994+TecExpo@users.noreply.github.com>
2025-02-11 08:16:55 +0530

    Update userModel.js

--- database/nosql/userModel.js
+++ database/nosql/userModel.js
@@ -1,2 +1,10 @@
-├── nosql/            # MongoDB models
-│   │   ├── userModel.js
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    passwordHash: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("User", UserSchema);
