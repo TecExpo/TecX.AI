a41048ff7cdae688a5d8fea5dd8e1daabac06b56
TecExpo <142141994+TecExpo@users.noreply.github.com>
2025-02-12 07:13:19 +0530

    Update producer.py

--- devops/kafka-zookeeper/producer.py
+++ devops/kafka-zookeeper/producer.py
@@ -1,1 +1,15 @@
+from kafka import KafkaProducer
+import json
 
+producer = KafkaProducer(
+    bootstrap_servers='kafka:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+def send_message(topic, message):
+    producer.send(topic, message)
+    producer.flush()
+    print(f"Sent: {message}")
+
+if __name__ == "__main__":
+    send_message("test-topic", {"event": "New user registered"})
