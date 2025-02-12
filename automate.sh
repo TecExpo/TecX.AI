bae08b54c040a5c4c6bf4aa989b8020f873cc3f6
TecExpo <142141994+TecExpo@users.noreply.github.com>
2025-02-12 09:04:26 +0530

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -1,4 +1,8 @@
 bash
 chmod +x scripts/deploy.sh
-
 ./scripts/deploy.sh
+chmod +x scripts/backup/*.sh
+./scripts/backup/db_backup.sh
+./scripts/backup/log_backup.sh
+./scripts/backup/ipfs_backup.sh
+
